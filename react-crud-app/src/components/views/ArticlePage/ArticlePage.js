5f6318bb0c9c1806318b28dacef3d61c719ce115
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-09-26 00:37:29 +0900

    ArticlePage가 보이지가 않아ㅏㅏㅏㅏㅏ

--- react-crud-app/src/components/views/ArticlePage/ArticlePage.js
+++ react-crud-app/src/components/views/ArticlePage/ArticlePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ArticlePage = ({ match, location }) => {
-    console.log(match.params.articleId)
+    console.log(match.params.articleId + "articlePage 확인")
     return (
         <div>
             ArticlePage-{match.params.articleId}
