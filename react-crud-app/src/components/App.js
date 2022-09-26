9638d60ae1d439490ac6ec01a97438cf058ade2c
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-09-27 01:53:46 +0900

    다른건 다되는데 왜 화면 이동만 안되니...

--- react-crud-app/src/components/App.js
+++ react-crud-app/src/components/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import BoardPage from './views/BoardPage/BoardPage'
 import ArticlePage from './views/ArticlePage/ArticlePage';
 import RegisterPage from './views/RegisterPage/RegisterPage';
+import { Redirect } from 'react-router';
 
 import "../App.css";
 
 function App() {
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<BoardPage />} />
-        <Route path="/article/:articleId/" element={<ArticlePage />} component={<ArticlePage/>} />
+        <Route path="/article/:articleId/" element={<ArticlePage />} />
         <Route path="/register/" element={<RegisterPage />} />
       </Routes>
     </div>
--- react-crud-app/src/index.js
+++ react-crud-app/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { Router} from 'react-router';
-import  history  from './utils/history';
+import history from './utils/history';
 import store from './store';
+import { BrowserRouter as Router, useNavigate } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <Router history={history} location={history.location}>
-      <App/>
+      <App />
     </Router>
   </Provider>
 );
