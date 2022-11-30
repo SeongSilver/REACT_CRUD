733d1cfc7d23ed7aa43c97224980d7f26221afea
BABY\SEONGEUN <yooseongeun@naver.com>
2022-11-30 23:42:13 +0900

    history 수정

--- react-crud-app/src/utils/history.js
+++ react-crud-app/src/utils/history.js
@@ -1,3 +1,11 @@
 import { createBrowserHistory } from 'history';
 
 export default createBrowserHistory();
+
+/* 
+    history 패키지는 임의의 환경(브라우저 포함) 에서 구동되는 
+    JavaScript 애플리케이션에서 세션 히스토리(방문 기록)의 관리와 
+    내비게이션 등을 쉽게 할 수 있도록 도와주는 라이브러리이다
+
+    
+*/
